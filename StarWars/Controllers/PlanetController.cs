95a3c93d7ced30c335a98fa4ad343b17d5326991
Em Ma <emilymtsn3@gmail.com>
2022-02-09 20:51:43 -0600

    Add project files.

---
+++ StarWars/Controllers/PlanetController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Flurl.Http;
+using StarWars.Models;
+
+namespace StarWars.Controllers
+{
+    public class PlanetController : Controller
+    {
+        List<Planet> _planetCollection;
+        // GET: PlanetController
+        [HttpGet]
+        public ActionResult Index()
+        {
+            string apiUri = "https://swapi.dev/api/planets/";
+            var apiTask = apiUri.GetJsonAsync<Results>();
+            apiTask.Wait();
+            _planetCollection = apiTask.Result.results;
+            apiTask.Wait();
+
+            _planetCollection = apiTask.Result.results;
+
+            return View(_planetCollection);
+        }
+
+        [HttpPost]
+        public ActionResult Index(string pageNumber = "1")
+        {
+
+            string apiUri = $"https://swapi.dev/api/planets/?page={pageNumber}";
+            var apiTask = apiUri.GetJsonAsync<Results>();
+            apiTask.Wait();
+            _planetCollection = apiTask.Result.results;
+
+            return View(_planetCollection);        
+        }
+        // GET: PlanetController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: PlanetController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PlanetController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PlanetController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PlanetController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PlanetController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PlanetController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ StarWars/Models/Planet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarWars.Models
+{
+    public class Planet
+    {
+        public string name { get; set; }
+        public string rotation_period { get; set; }
+        public string orbital_period { get; set; }
+        public string diameter { get; set; }
+        public string climate { get; set; }
+        public string gravity { get; set; }
+        public string terrain { get; set; }
+        public string surface_water { get; set; }
+        public string population { get; set; }
+    }
+}
